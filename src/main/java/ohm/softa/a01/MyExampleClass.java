4fe02c83fad69ee7dc396488762a843f5d05c237
Baudster <bauderse68422@th-nuernberg.de>
2019-03-28 10:42:25 +0100

    Finalized multiply method

--- src/main/java/ohm/softa/a01/MyExampleClass.java
+++ src/main/java/ohm/softa/a01/MyExampleClass.java
@@ -5,8 +5,8 @@ public class MyExampleClass {
 	 * Basic integer multiplication
 	 * @return product of a and b
 	 */
-	public int multiply(int a, int b) {
-		// TODO return correct multiplication result
-		return 0;
+	public int multiply(int a, int b)
+	{
+		return a * b;
 	}
 }
